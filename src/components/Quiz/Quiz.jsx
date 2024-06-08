f7b3c31965998d620dbc88940b9da630ddaab343
Barbora Nováková <barun.novak@gmail.com>
2024-06-08 18:49:25 +0200

    uprava komponenty Top a Quiz

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Quiz.css';
+import { useNavigate } from 'react-router-dom';
 
 export const Quiz = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/quiz');
+  };
   return (
     <>
       <section className="test">
         <div className="container-test">
           <h2>Udělej si test, jaký záchod je přímo pro tebe</h2>
 
-          <button className="test-btn">Vyplnit test</button>
+          <button onClick={handleClick} className="test-btn">
+            Vyplnit test
+          </button>
         </div>
       </section>
     </>
