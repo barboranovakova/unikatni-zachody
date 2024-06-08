0df7a0966f56ca3549dba148ca9a88c26e018d7d
Vardui Saribekian <vartvioline@gmail.com>
2024-06-08 10:44:16 +0200

    Zakladani komponent

--- unikatni-zachody/src/index.jsx
+++ unikatni-zachody/src/index.jsx
@@ -1,7 +1,33 @@
 import { createRoot } from 'react-dom/client';
 import { HomePage } from './pages/HomePage';
 import './global.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from 'react';
 
-createRoot(
-  document.querySelector('#app'),
-).render(<HomePage />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <HomePage />,
+      },
+      {
+        path: 'map',
+        element: <Map />,
+      },
+      {
+        path: 'quiz',
+        element: <Quiz />,
+      },
+      {
+        path: 'toilett_detail',
+        element: <ToilettDetail />,
+      },
+    ],
+  },
+]);
+createRoot(document.querySelector('#app')).render(
+  <RouterProvider router={router} />,
+);
