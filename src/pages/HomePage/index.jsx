deac5f525f90f6bab468cf3be4cce23eb0d1b847
Barbora Nováková <barun.novak@gmail.com>
2024-06-08 12:03:38 +0200

    import komponent jako obsah stranky

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,16 @@
 import './style.css';
+import { Banner } from '../../components/Banner/Banner';
+import { Top } from '../../components/Top/Top';
+import { FunFacts } from '../../components/FunFacts/FunFacts';
+import { Quiz } from '../../components/Quiz/Quiz';
 
 export const HomePage = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Banner />
+      <Top />
+      <FunFacts />
+      <Quiz />
+    </div>
+  );
 };
